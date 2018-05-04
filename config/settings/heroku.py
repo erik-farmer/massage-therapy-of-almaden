69e82ae538b7f143e1d9756b1a46ed89b804aecf
Erik Farmer <5082725+erik-farmer@users.noreply.github.com>
2018-05-03 17:28:57 -0700

    add logfile line to Procfile, add secret key to rotate on each deploy

--- config/settings/heroku.py
+++ config/settings/heroku.py
@@ -1,14 +1,8 @@
+import secrets
 from .base import *
 
 
-# Set Security Key via heroku cli
-# CREATING SECURE KEYS:
-"""
-import secrets
-secrets.token_urlsafe(some_length)
-"""
-# heroku config:set SECRET_KEY=somethingstrong
-
+SECRET_KEY = secrets.token_urlsafe(50)
 
 # Configure Django App for Heroku.
 import django_heroku
