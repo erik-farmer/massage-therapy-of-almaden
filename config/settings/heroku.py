7ab59b0dd8e3c06381660e11219a8af74bd66503
Erik Farmer <5082725+erik-farmer@users.noreply.github.com>
2018-05-05 21:14:26 -0700

    reset migrations after dev phase, modify heroku settings

--- config/settings/heroku.py
+++ config/settings/heroku.py
@@ -1,11 +1,25 @@
 from .base import *
 import secrets
-
+import os
 
 DEBUG = False
+REDIS_URL = os.environ.get('REDIS_URL')
 
 
 SECRET_KEY = secrets.token_urlsafe(50)
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": REDIS_URL,
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        },
+        "KEY_PREFIX": "mtoa"
+    }
+}
+
+DEFENDER_REDIS_URL = REDIS_URL
 
 # Configure Django App for Heroku.
 import django_heroku
